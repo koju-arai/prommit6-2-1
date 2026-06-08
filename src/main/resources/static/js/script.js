ed13a4b4292f4bfaafbf56eff93a11f2e108d110
koju <fine.fantasy.fanny@gmail.com>
2026-06-08 23:20:08 +0900

    update logic

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,94 +1,148 @@
-$all(".data-refinement .switch").forEach((switchEl)=>{
-  switchEl.addEventListener("click",(event)=>{
+if($(".data-refinement")){
+	$all(".data-refinement .switch").forEach((switchEl)=>{
+	  switchEl.addEventListener("click",(event)=>{
+		
+	    const contentEl = switchEl.nextElementSibling;
+		
+		contentEl.classList.toggle("open");
+		
+		$all(".data-refinement .switch").forEach((el)=>{
+		  const content = el.nextElementSibling;
+		  
+		  if(content !== contentEl){
+			content.classList.remove("open")
+		  };
+		  if(content.classList.contains("open")){
+	        content.style.display = "block";	    
+		  } else {
+			content.style.display = "none";
+		  };
+	    });
+	  });
+	});
 	
-    const contentEl = switchEl.nextElementSibling;
-	
-	contentEl.classList.toggle("open");
-	
-	$all(".data-refinement .switch").forEach((el)=>{
-	  const content = el.nextElementSibling;
+	$all(".data-refinement .cancel").forEach((switchEl)=>{
 	  
-	  if(content !== contentEl){
-		content.classList.remove("open")
-	  };
-	  if(content.classList.contains("open")){
-        content.style.display = "block";	    
-	  } else {
-		content.style.display = "none";
-	  };
-    });
-  });
-});
+	  switchEl.addEventListener("click",()=>{
+		
+		$all(".data-refinement .switch").forEach((el)=>{
+		  const content = el.nextElementSibling;
+		  content.classList.remove("open");
+		  content.style.display = "none";
+	    });
+		
+	  });
+	});
+	
+	tagManage();
+	tagSwitch();
+}
 
-$all(".data-refinement .cancel").forEach((switchEl)=>{
-  
-  switchEl.addEventListener("click",()=>{
+if($(".memo-menu")){
+	$all(".memo-menu .menu-switch").forEach((switchEl)=>{
+	  switchEl.addEventListener("click",(event)=>{
+		
+	    const contentEl = switchEl.nextElementSibling;
+		
+		contentEl.classList.toggle("open");
+		
+		$all(".memo-menu .menu-switch").forEach((el)=>{
+		  const content = el.nextElementSibling;
+		  
+		  if(content !== contentEl){
+			content.classList.remove("open")
+		  };
+		  if(content.classList.contains("open")){
+	        content.style.display = "block";	    
+		  } else {
+			content.style.display = "none";
+		  };
+	    });
+	  });
+	});
 	
-	$all(".data-refinement .switch").forEach((el)=>{
-	  const content = el.nextElementSibling;
-	  content.classList.remove("open");
-	  content.style.display = "none";
-    });
+	$all(".menu-box .delete-switch").forEach((switchEl)=>{
+	  switchEl.addEventListener("click",(event)=>{
+		
+	    const contentEl = $(".delete-dialog" ,switchEl.closest(".memo-menu"))
+		
+		contentEl.classList.toggle("open");
+		if(contentEl.classList.contains("open")) $(".overlay").style.display = "block";
+		
+		$all(".delete-dialog").forEach((el)=>{
+		  
+		  if(el !== contentEl){
+			el.classList.remove("open")
+		  };
+		  if(el.classList.contains("open")){
+	        el.style.display = "block";	    
+		  } else {
+			el.style.display = "none";
+		  };
+	    });
+	  });
+	});
 	
-  });
-});
+	$all(".delete-dialog .delete-cancel").forEach((switchEl)=>{
+	  switchEl.addEventListener("click",(event)=>{
+		
+		$all(".delete-dialog").forEach((el)=>{
+		  el.classList.remove("open");
+		  el.style.display = "none";
+		})
+		$(".overlay").style.display = "none";
+	   
+	  });
+	});
+}
 
-$all(".memo-menu div.switch").forEach((switchEl)=>{
-  switchEl.addEventListener("click",(event)=>{
+if($(".form-style")){
+	tagManage();
+	tagSwitch();
+};
+function tagSwitch(){
+	const switchEl= $(".tag-switch")
+	  switchEl.addEventListener("click",()=>{
+	    const contentEl = $(".tags")		
+		contentEl.classList.toggle("open");
+	    if(contentEl.classList.contains("open")){
+          contentEl.style.display = "block";	    
+	    } else {
+		  contentEl.style.display = "none";
+	    };
+	  });		   
+};
+function tagManage(){
+	const tagCheckBoxes = $all('input[name="tagIds"]');
+	const selectedTags = $(".selected-tags");
 	
-    const contentEl = switchEl.nextElementSibling;
-	
-	contentEl.classList.toggle("open");
-	
-	$all(".memo-menu div.switch").forEach((el)=>{
-	  const content = el.nextElementSibling;
-	  
-	  if(content !== contentEl){
-		content.classList.remove("open")
-	  };
-	  if(content.classList.contains("open")){
-        content.style.display = "block";	    
-	  } else {
-		content.style.display = "none";
-	  };
-    });
-  });
-});
+	function updatedSlectedTags(){
+		selectedTags.innerHTML = '<span class="tag-switch">▼</span>';
+		[...tagCheckBoxes]
+		.filter(checkbox => checkbox.checked)
+		.forEach(checkbox => {
+		  const htmlTagCheckedBox = `
+		    <li class="selected-tag">
+				<span class="tag-cancel">×</span>
+				<span class="selected-tag-name">${checkbox.dataset.tagName}</span>	    
+			</li>
+		  `;
+		  const tagCheckedBox = htmlStrToSingleElement(htmlTagCheckedBox);
+		  const tagCancel =  $(".tag-cancel", tagCheckedBox);
+		  tagCancel.addEventListener("click", ()=>{
+			checkbox.checked = false;
+			updatedSlectedTags();
+		  });
+		  selectedTags.append(tagCheckedBox);
+		});
+	};
+			
+	tagCheckBoxes.forEach(checkBox => {
+		checkBox.addEventListener("change",updatedSlectedTags);
+	});
+	updatedSlectedTags();
 
-$all(".menu-box .switch").forEach((switchEl)=>{
-  switchEl.addEventListener("click",(event)=>{
-	
-    const contentEl = $(".delete-dialog" ,switchEl.closest(".memo-menu"))
-	
-	contentEl.classList.toggle("open");
-	if(contentEl.classList.contains("open")) $(".overlay").style.display = "block";
-	
-	$all(".delete-dialog").forEach((el)=>{
-	  
-	  if(el !== contentEl){
-		el.classList.remove("open")
-	  };
-	  if(el.classList.contains("open")){
-        el.style.display = "block";	    
-	  } else {
-		el.style.display = "none";
-	  };
-    });
-  });
-});
-
-$all(".delete-dialog .delete-cancel").forEach((switchEl)=>{
-  switchEl.addEventListener("click",(event)=>{
-	
-	$all(".delete-dialog").forEach((el)=>{
-	  el.classList.remove("open");
-	  el.style.display = "none";
-	})
-	$(".overlay").style.display = "none";
-   
-  });
-});
-
+};
 function $(selectorText, parentElement = document){
     const el = parentElement?.querySelector(selectorText);
     return el;
@@ -100,4 +154,9 @@ function $all(selectorText, parentElement = document){
 function safeText(text, selectorText, parentElement = document){
     const el = $(selectorText, parentElement);
     if(el) el.textContent = text ?? "";
+};
+function htmlStrToSingleElement(htmlStr){
+    const dummyDiv = document.createElement("div");
+    dummyDiv.innerHTML = htmlStr;
+    return dummyDiv.firstElementChild;
 };
